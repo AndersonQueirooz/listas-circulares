860c30bcaba09c428691d217a95844534d8b701e
AndersonQueirooz <andersonqueiroz537@gmail.com>
2026-02-13 15:55:13 -0300

    finalizando-metodos

---
+++ Listas-Circulares/src/ListaCircular.java
@@ -0,0 +1,82 @@
+public class ListaCircular <T>{
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    
+    public ListaCircular() {
+        this.cabeca = null;
+        this.cauda = null;
+        this.tamanhoLista = 0;
+    }
+
+    public int size() {
+        return this.tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return this.tamanhoLista == 0 ? true : false;
+    }
+
+    private No<T> getNo(int index) {
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+        if (index == 0) {
+            return this.cauda;
+        }
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < index); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+
+    }
+    
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+    
+    public void remove(int index) {
+        if (index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice é maior do que o tamanho da lista");
+        No<T> noAuxiliar = this.cauda;
+        if (index == 0) {
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        } else if (index == 1) {
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        } else {
+            for (int i = 0; (i < index - 1); i++) {
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
+    public void add(T conteudo) {
+        No<T> novoNo = new No<>(conteudo);
+        if (this.isEmpty()) {
+            this.cauda = novoNo;
+            this.cabeca = this.cauda;
+            this.cabeca.setNoProximo(this.cauda);
+        } else {
+            novoNo.setNoProximo(this.cauda);
+            this.cabeca.setNoProximo(novoNo);
+            this.cabeca = novoNo;
+        }
+        this.tamanhoLista++;
+    }
+
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < this.size()); i++) {
+            strRetorno += noAuxiliar.toString() + "->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += this.isEmpty() ? "null" : "(retorna ao início)";
+        return strRetorno;
+    }
+}
